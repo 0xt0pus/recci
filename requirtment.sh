00d392b76a5f2fa5d8328ed71a126471123c15e2
Muhammad Yaqoob <55015567+whitehatyaqoob@users.noreply.github.com>
2022-05-31 09:32:20 +0500

    Update requirtment.sh

--- requirtment.sh
+++ requirtment.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+sudo apt-get update
 sudo apt install figlet toilet 
 sudo apt-get install ghostscript
 sudo apt-get install enscript 
 sudo apt-get install sublist3r
+sudo apt-get install gobuster
 cd /usr/share
 git clone https://github.com/xero/figlet-fonts
 mv figlet-fonts/* figlet && rm -rf figlet-fonts
